9149620633a6ddc9a562fc83299725422b57e0b9
felmartins <61277213+felmartins@users.noreply.github.com>
2021-02-02 18:50:01 +0100

    Add files via upload

--- AutRelChecker.r
+++ AutRelChecker.r
@@ -9,9 +9,9 @@ library(readxl)
 library(irr)
 
 AutRelChecker <- function(x, y, s.column = 1) {
-  cat("### Automatic Reliability Checker for Nominal Data - FBSM ###\n")
-  cat("### Version 1.0 - Highly unstable and under testing, comments refer to line(s) above ###\n")
-  cat("### Code Revision C - Use github for updated version and changelog ###\n\n")
+  cat("### Automatic Reliability Checker for Nominal Data - Felipe Martins ###\n")
+  cat("### Version 1.0b - Revised and Functional, Source comments refer to line(s) above ###\n")
+  cat("### Code Revision F - https://github.com/felmartins/AutRelChecker for updated version and changelog ###\n\n")
   Sys.sleep(4)
   cat("Starting Analysis...\n\n")
   reliabilityresults <- NULL
@@ -44,7 +44,7 @@ AutRelChecker <- function(x, y, s.column = 1) {
   rownames(reliabilityresults) <- names[s.column:length(x)]
   reliabilityresults <- as.data.frame(reliabilityresults)
   write.csv(reliabilityresults, file = "AutRelChecker_AnalysisResults.csv")
-  cat("### Analysis Complete - Print of Results or Saved Object to Follow ###\nData is also saved in a CSV file on your working directory\n\n")
+  cat("Analysis Complete - Print of Results or Saved Object to Follow... \n Data is also saved in a CSV file on your working directory\n\n")
   ### Add column names from x to resulting object rows
   return(reliabilityresults)
   ### Function return statement - object is a dataframe containing values for Gwet AC1, Cohen Kappa and Simple Agreement
